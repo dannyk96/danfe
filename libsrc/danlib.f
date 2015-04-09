e4e2da82b17fb396c0351bd85c1f2400dc91f8ed
dannyk6 <daniel.kidger@gmail.com>
2015-04-09 17:16:21 +0100

    Initial create: this concats lots of source so can make just one object file (also lets us check subroutine naming collisions)

---
+++ libsrc/danlib.f
@@ -0,0 +1,13 @@
+!
+!  One way of putting all teh danfe libraries into one compile 
+! hence one .o file
+!
+      include "keyword.f"
+      include "k_mesh.f"
+      include "k_meshio.f"
+      include "k_bc.f"
+      include "elements.f"
+      include "general.f"
+      include "small.f"
+      include "gauss.f"
+      include "matrix.F"
